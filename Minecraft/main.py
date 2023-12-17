1b83331ee3d863f942e483ffb39bb992c6b49495
NaturalTwitch <37255324+NaturalTwitch@users.noreply.github.com>
2023-12-17 16:26:15 -0500

    Update main.py
    Added announcer for comments that aren't commands, automatically thanks the gifter, and thanks sharers

--- Minecraft/main.py
+++ Minecraft/main.py
@@ -2,14 +2,22 @@ from TikTokLive import TikTokLiveClient
 from TikTokLive.types.events import CommentEvent, ConnectEvent, GiftEvent, ShareEvent, LikeEvent
 import pyautogui
 import time
+import PySimpleGUI as sg
+import pyttsx3
 import configparser
 import os
+
+lang = 'en'
+engine = pyttsx3.init()
+engine.setProperty('language', lang)
+engine.setProperty('rate', 150)
 
 config = configparser.ConfigParser()
 
 # Check if the config.txt file exists
 if not os.path.exists('config.txt'):
     # The file does not exist, create it with predefined settings
+    sg.Popup("Warn", "No Config.txt Found. Making One")
     print(f'No Config.txt found. Making One')
 
     config.read_dict({'TikTok': {'username': 'your_username'}})
@@ -28,7 +36,7 @@ client: TikTokLiveClient = TikTokLiveClient(unique_id=f"@{tiktok_username}")
 # Define how you want to handle specific events via decorator
 @client.on("connect")
 async def on_connect(_: ConnectEvent):
-    print("Connected to Room ID:", client.room_id)
+    print("Connected to Streamer:", tiktok_username)
 
 
 @client.on("like")
@@ -56,6 +64,9 @@ async def on_gift(event: GiftEvent):
     # Streakable gift & streak is over
     if event.gift.streakable and not event.gift.streaking:
         print(f"{event.user.unique_id} sent {event.gift.count}x \"{event.gift.info.name}\"")
+
+        engine.say(f"Thanks for the {event.gift.info.name} {event.user.nickname}")
+        engine.runAndWait()
 
         if event.gift.info.name == "Rose":
             pyautogui.keyDown("w")
@@ -97,7 +108,6 @@ async def on_gift(event: GiftEvent):
         print(f"{event.user.unique_id} sent \"{event.gift.info.name}\"")
 
 
-# Notice no decorator?
 async def on_comment(event: CommentEvent):
     print(f"{event.user.nickname} -> {event.comment}")
 
@@ -162,6 +172,9 @@ async def on_comment(event: CommentEvent):
     if event.comment == "e":
         pyautogui.press('e')
         return
+
+    engine.say(f"{event.user.unique_id} said {event.comment}")
+    engine.runAndWait()
 
 
 # Define handling an event via a "callback"
